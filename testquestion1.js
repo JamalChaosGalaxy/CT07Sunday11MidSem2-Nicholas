8c1ab966fb05445541f8dc3d34a1ee0b0da46c01
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-08-31 03:40:54 +0000

    cto7sunday11midsem2-nicholas

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,17 @@
+let ball;
+function setup(){
+    new Canvas(600, 400);
+    background(220);
 
+    world.gravity.y = 10;
+}
+function draw(){
+    background(220);
+    if(mouse.presses('right') || mouse.presses('left')){
+        ball = new Sprite();
+        ball.x = mouse.x;
+        ball.y = mouse.y;
+        ball.radius = 20;
+        ball.collider = 'dynamic'; 
+    }
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,23 @@
+let ball;
+let floor;
+function setup(){
+    new Canvas(600, 400);
 
+
+    world.gravity.y = 10;
+    floor = new Sprite(0, 400, 1400, 30);
+    floor.colour = "blue";
+    floor. collider = 'static';
+
+}
+function draw(){
+    background(220);
+    if(mouse.presses('right') || mouse.presses('left')){
+        ball = new Sprite();
+        ball.x = mouse.x;
+        ball.y = mouse.y;
+        ball.radius = 20;
+        ball.collider = 'dynamic'; 
+        ball.bounciness = 0.5;
+    }
+}
